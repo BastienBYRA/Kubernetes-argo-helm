6ca9e31cd31f962a0347586497b27bdad4adfb23
Bastien <neitsabast@gmail.com>
2024-10-24 16:37:54 +0200

    update

--- manual/run.sh
+++ manual/run.sh
@@ -6,12 +6,12 @@ helm repo update
 
 # Deploie le Kube state metrics
 helm install ksm-rel prometheus-community/kube-state-metrics --create-namespace -n ksm-ns
-helm install prometheus-rel prometheus-community/prometheus --create-namespace -n prometheus-ns -f ./values-prometheus.yml
+helm install prometheus-rel prometheus-community/prometheus --create-namespace -n prometheus-ns -f ../config/values-prometheus.yml
 
 # Installe repo Grafana (et Loki)
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm install grafana-rel grafana/grafana --create-namespace -n grafana-ns -f ./values-grafana.yml
+helm install grafana-rel grafana/grafana --create-namespace -n grafana-ns -f ../config/values-grafana.yml
 
 helm repo add vm https://victoriametrics.github.io/helm-charts
 helm repo update
