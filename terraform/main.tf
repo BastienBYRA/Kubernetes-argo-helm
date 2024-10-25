2b3ef4a37280d45361d38bd2bd7dd6ad7f895644
Bastien <neitsabast@gmail.com>
2024-10-25 10:22:10 +0200

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,31 +7,39 @@ provider "helm" {
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo/argo-cd"
+  chart      = "argo-cd"
+  create_namespace = true
+  namespace = "argocd"
 }
 
-resource "helm_release" "kube_state_metrics" {
-  name       = "kube_state_metrics"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-community/kube-state-metrics"
-}
+# resource "helm_release" "kube_state_metrics" {
+#   name       = "kube-state-metrics"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-state-metrics"
+#   create_namespace = true
+#   namespace = "ksm"
+# }
 
-resource "helm_release" "prometheus" {
-  depends_on = [helm_release.kube_state_metrics]
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-community/prometheus"
-  values = [
-    "${file("../config/values-prometheus.yml")}"
-  ]
-}
+# resource "helm_release" "prometheus" {
+#   depends_on = [helm_release.kube_state_metrics]
+#   name       = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "prometheus"
+#   values = [
+#     "${file("../config/values-prometheus.yml")}"
+#   ]
+#   create_namespace = true
+#   namespace = "prometheus"
+# }
 
-resource "helm_release" "grafana" {
-  depends_on = [helm_release.prometheus]
-  name       = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "grafana/grafana"
-  values = [
-    "${file("../config/values-grafana.yml")}"
-  ]
-}
+# resource "helm_release" "grafana" {
+#   depends_on = [helm_release.prometheus]
+#   name       = "grafana"
+#   repository = "https://grafana.github.io/helm-charts"
+#   chart      = "grafana"
+#   values = [
+#     "${file("../config/values-grafana.yml")}"
+#   ]
+#   create_namespace = true
+#   namespace = "grafana"
+# }
